e5701f3065ab08ef03c7e7ce0aabaff2d65cac33
karoluch <karol_konaszynski@wp.pl>
2010-11-30 11:12:11 +0100

    Troche dopisalem do Image i poczatek pliku findCorners. Nic wielkiego

--- image.C
+++ image.C
@@ -1,4 +1,7 @@
 #include "image.h"
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
 
 Image::Image(int w, int h)
 {
@@ -19,7 +22,7 @@ Image::~Image() {
     delete data;
 }
 
-Image &operator=(const Image &im)
+void Image::operator=(const Image &im)
 {
     if(width != im.width || height != im.height)
     {
@@ -32,11 +35,67 @@ Image &operator=(const Image &im)
     memcpy(data, im.data, width*height);
 }
 
-Image Image::readPGM(const char *filename)
+Image Image::readPGM(const char *filename) //Karol's
 {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) printf("unable to open %s\n", filename);
+    char* buf = new char[1024]; int bufsize = 1024;
+    char tmp[100];
+    int width, height;
+    for (int i=0;;)
+    {
+        getline(&buf, (size_t*)&bufsize, file);
+        if (buf[0] == '#') continue;
+        if (i == 0) ;
+        else if (i == 1) sscanf(buf, "%d %d", &width, &height);
+        else if (i == 2) break;
+        i ++;
+    }
+    
+    // fprintf(stderr, "%d %d\n", width, height);
+    Image* ret = new Image(width, height);
+    for (int i=0; i<height; i++)
+        for  (int j=0; j<width; j++)
+            fscanf(file, "%c", &(*ret)[i][j]);
+    
+    return *ret;
 }
 
-void Image::writePGM(void) const
+void Image::writePGM(FILE* file) const
 {
+    fprintf(file, "P5\n%d %d\n255\n", width, height);
+    for (int i=0; i<height; i++)
+        for  (int j=0; j<width; j++)
+            fprintf(file, "%c", data[i*width+j]);
 }
 
+/* Karol's part */
+
+void Image::move(int dw, int dh)
+{
+    uint8_t* newdata = new uint8_t[width*height];
+    for (int i=0; i<height; i++)
+        for (int j=0; j<width; j++)
+        {
+            int ind = width*(i-dh) + j-dw;
+            newdata[i*width+j] = i-dh >= 0 && j-dw >=0 && i-dh < height && j-dw < width ? data[ind] : 0;
+        }
+    memcpy(data, newdata, width*height);
+}
+
+void Image::randomInit()
+{
+    for (int i=0; i<height; i++)
+        for (int j=0; j<width; j++)
+            data[i*width+j] = rand()&255;
+}
+
+Image &Image::difference(const Image& im) const //wartość bezwzględna różnicy między dwoma obrazkami, pixel po pixelu
+{
+    Image* ret = new Image(*this);
+    for (int i=0; i<height; i++) 
+        for (int j=0; j<width; j++)
+            (*ret)[i][j] = (uint8_t)abs((*ret)[i][j] , im[i][j]);
+    return *ret;
+}
+    
--- image.h
+++ image.h
@@ -1,4 +1,7 @@
 #include <stdint.h>
+#include <cstring>
+#include <cstdio>
+
 
 class Image
 {
@@ -10,13 +13,25 @@ public:
     Image(const Image &im);
     ~Image();
 
-    Image &operator=(const Image &im);
-    uint8_t* operator[](int row) { return data + idx*w; }
-    const uint8_t *operator[](int row) const { return data + idx*w; }
-
+    void operator=(const Image &im);
+    uint8_t* operator[](int row) { return data + row*width; }
+    const uint8_t *operator[](int row) const { return data + row*width; }
+    
     bool isOpaque(int x, int y) const { return (*this)[y][x] != 0; }
 
     static Image readPGM(const char *filename);
-    void writePGM(void) const;
+    void writePGM(FILE* file = stdin) const;
+    
+    /* Karol's */
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+    void move(int dw, int dh); //very not const
+    void randomInit(); //very not const
+    Image &difference(const Image& im) const;
+    inline uint8_t abs(uint8_t a, uint8_t b) const { return a<b ? b-a : a-b; }
+    Image contour() const { Image img2(*this); img2.move(1,1); return this->difference(img2); }
+    static Image contour(const char* filename) { return readPGM(filename).contour(); }
+    void writePGM(const char* filename) { FILE* file = fopen(filename, "w"); writePGM(file); }
+    
 };
 
