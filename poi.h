c92e9253541c9ecc2e9be79c6f2a9d5dd211f2cf
wixor <wixorpeek@gmail.com>
2011-01-22 13:08:29 +0100

    cleaup: poi

--- poi.h
+++ poi.h
@@ -3,16 +3,22 @@
 
 #include <cmath>
 #include <vector>
+
+#include "util.h"
 #include "image.h"
+
+struct POI : public Point
+{
+    float val;
+
+    inline POI() { }
+    inline POI(float x, float y, float val) : Point(x,y), val(val) { }
+    inline POI(const POI &p) : Point(p.x, p.y), val(p.val) { }
+};
 
 Array2D<float> evaluateImage(const Image &src, int steps, const std::vector<float> &scales);
 Image reduceEvaluationToImage(const Array2D<float> &eval);
 std::vector<POI> findPOIs(const Array2D<float> &eval, float threshold, int count, float param);
 std::vector<POI> findPOIs(const Array2D<float> &eval, float threshold, int count);
-void overlayPOIs(const std::vector<POI> &pois, Image *dst);
-Image renderPOIs(const std::vector<POI> &pois, int w, int h);
-
-Array2D<float> gaussianBlur(const Image& img, float deviation, int border);
-Image renderGaussianDifference(const Image& img1, const Image img2, float deviation, int border, bool smart);
 
 #endif
